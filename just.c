7ff377b167a1806dd1fe8f2307879948c67cf59a
Abhi J <abhijai3@cisco.com>
2019-09-18 10:35:33 +0530

    First Draft

---
+++ just.c
@@ -0,0 +1,78 @@
+/*Hello World! that*/#include"allFriendsPeersManagersDirectorsEt.Al."
+/*let me*/#define some_cliche_aphorism_which_needless_to_say_is TRUE
+
+//==================================
+/*CHANGE IS THE ONLY*/const;
+//==================================
+
+/*Of course there will be a*/void/*for some time but 
+the*/main()/*point of this CHANGE is to think about the*/{ 
+  short _and_the;long _term_goals;
+  while(_I){/*had always pondered*/ 
+    if (_I){/*were to write my last-day Note 
+    should _I be creative & meaningful? or
+    should _I*/do{/*what is conventionally followed?*/
+    }while (_I)/*was at it...*/; 
+    /*"an epiphany" like a lightening*/struct{}/*me...pun intended;);)*/;
+    goto CODING;/*yes! goto CODING was the realization*/
+    /*and _I let all my*/volatile _thoughts;
+    /*that were hitherto a*/float _become;static _and; 
+    register _just_with_this_idea;for(_my;_last_day;_email){};
+    }
+  }
+
+  if(_I)/*were to*/{enum/*erate*/ my_career_in_cisco{
+    /*_I started as a*/
+    DEVELOPER,
+    /*then became a*/
+    MANAGER,//to the Automation Team
+    /*and then for a brief stint was a*/
+    TECHNICAL_LEADER
+    };
+  }
+
+  auto/*mation has always*/ _been_a; double _edged_sword;
+
+  /*juggling some*/extern/*al and some*/int/*ernal*/_dilemmas;
+
+  do{/*_I need to*/switch (_carrers){//and look for alternate fields//??
+    //or hone my skills??
+    //aquire new skill(s)??
+    /*or I just*/goto CODING;//??
+    /*or take a*/break;for(;_a;){while(_and_travel){}};//??
+    /*How*/long _would_that_break_be=/*yes! precisely*/sizeof("that_break");//??
+    /*As I ponder on this*/case(_and_others):
+    /*life must*/continue;
+    /*I know my*/default:/*love is*/CODING:;
+    }
+  }while(_these_dilemmas_are </*less than*/_other_worries_in_life);
+  for(;_now;)/*let me*/{continue;}/*and skip solving this*/RIDDLE();
+  /*_I*/do{/*believe that the last-day Note ought to be*/
+    signed _off;/*with the mention of few eminent*/ 
+    char/*acters*/ *_who_trusted_supported_helped_me_in_this_journey[] = {
+      /*I would like to thank*/
+      "NADI K",
+      "SANTOSH P",
+      "BALA T",
+      "MY TEAM",//automation
+      "EXTENDED TEAM",//@cspg
+      "PEERS",
+      "et.al"
+    };  
+    for(;_all;){}/*the opportunities that you paved my way*/
+  }while (_I);/*wholeheartedly wish luck*/for(;;)/*all of you*/
+  /*_I hope some good luck bounces back in*/return;
+
+  //Like this section of the code, I am also not reachable on 
+  union{};/*of (Facebook,Instagram,LinkedIn)*/
+  /*so*/if(_you)/*would like to reach me*/{
+  //mail me at abhins4@gmail.com
+  }else{
+  /*WhatsApp or Call me at +91-9611012995*/
+  }
+  /*A NOTE*/for(;;){}/*All The Geeks*/
+  //Compile & Execute this Note and answer the Easter Egg 
+  //do it NOW!
+  //as in:
+  //_abhi (pun intended)
+}
