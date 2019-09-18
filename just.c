f2acb1354c6ed0aa1f5fb769a6a74dec3b4c0270
Abhi J <abhijai3@cisco.com>
2019-09-18 16:01:44 +0530

    fixed trailing whitespaces and 80char limit

--- just.c
+++ just.c
@@ -3,24 +3,24 @@
 //let's just.c how it goes!
 
 /*Hello World! that*/#include"allFriendsPeersManagersDirectorsEt.Al."
-/*let me*/#define some_cliché_aphorism_which_needless_to_say_is TRUE
+/*let me*/#define some_cliché_aphorism_which_needless_to_say_is TRE
 
 //==================================
 /*CHANGE IS THE ONLY*/const;
 //==================================
 
-/*and of course there will be a*/void/*for some time but 
-the*/main()/*point of this CHANGE is to think about the*/{ 
+/*and of course there will be a*/void/*for some time but
+the*/main()/*point of this CHANGE is to think about the*/{
   short _and_the;long _term_goals;
-  while(_I){/*had always pondered*/ 
-    if(_I){/*were to write my last-day Note 
+  while(_I){/*had always pondered*/
+    if(_I){/*were to write my last-day Note
     should _I be creative & meaningful? or
     should _I*/do{/*what is conventionally followed?*/
-    }while(_I)/*was at it...*/; 
+    }while(_I)/*was at it...*/;
     /*"an epiphany" like a lightening*/struct{}/*me...pun intended;);)*/;
-    goto CODING;/*yes! goto CODING was the realization*/
+    goto CODING;/*yes! "goto CODING" was the realization*/
     /*and _I let all my*/volatile _thoughts;
-    /*that were hitherto a*/float _become;static _and; 
+    /*that hitherto were a*/float _become;static _and;
     register _just_with_this_idea;for(_my;_last_day;_email){};
     }
   }
@@ -37,7 +37,7 @@ the*/main()/*point of this CHANGE is to think about the*/{
 
   auto/*mation has always*/ _been_a;double _edged_sword;
 
-  /*juggling some*/extern/*al and some*/int/*ernal*/_dilemmas;
+  /*now juggling with some*/extern/*al and some*/int/*ernal*/_dilemmas;
 
   do{/*_I need to*/switch(_carrers){//and look for alternate fields//??
     //or hone my skills??
@@ -52,7 +52,7 @@ the*/main()/*point of this CHANGE is to think about the*/{
   }while(_these_dilemmas_are </*less than*/_other_worries_in_life);
   for(;_now;)/*let me*/{continue;}/*and skip solving this*/RIDDLE();
   /*_I*/do{/*believe that the last-day Note ought to be*/
-    signed _off;/*with the mention of few eminent*/ 
+    signed _off;/*with the mention of few eminent*/
     char/*acters*/ *_who_trusted_supported_helped_me_in_this_journey[] = {
       /*I would like to thank*/
       "NADI K",
@@ -62,20 +62,20 @@ the*/main()/*point of this CHANGE is to think about the*/{
       "EXTENDED TEAM",//@cspg
       "PEERS",
       "et.al"
-    };  
-    for(;_all;){}/*the opportunities that you paved my way*/
+    };
+    for(;_all;){}/*the opportunities that you paved my way.*/
   }while(_I);/*wholeheartedly wish luck*/for(;;)/*all of you*/
   /*_I hope some good luck bounces back in*/return;
 
-  //Like this section of the code, I am also not reachable on 
-  union{};/*of (Facebook,Instagram,LinkedIn)*/
+  //Like this section of the code, I am also unreachable on
+  /*social-media*/union{/*of(Facebook,Instagram,LinkedIn)*/};
   /*so*/if(_you)/*would like to reach me*/{
   //mail me at abhins4@gmail.com
   }else{
-  /*WhatsApp or Call me at +91-9611012995*/
+  //WhatsApp or Call me at +91-9611012995
   }
-  /*A NOTE*/for(;;){}/*All The Geeks*/
-  //Compile & Execute this Note verbatim and answer the Easter Egg 
+  /*a NOTE*/for(;;){}/*All The Geeks*/
+  //compile & execute this Note verbatim and answer the Easter Egg I embedded:
   //do it NOW!
   //as in:
   //_abhi (pun intended)
